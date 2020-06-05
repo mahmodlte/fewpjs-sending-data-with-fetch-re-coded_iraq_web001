8e2e7d58f7a6e2b7ff2ee350ee253ced828db72e
Mahmoud Mansour <mahmodlte@gmail.com>
2020-06-05 15:09:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+// Add your code here 
+const ulList = document.querySelector('#users-list');
+const fetchUrl = "http://localhost:3000/users"
+
+function submitData (userName, userEmail){
+    let users = {
+        name: userName,
+        email: userEmail
+    }
+    let configObj = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+
+        },
+        body: JSON.stringify(users)
+    }
+
+   return fetch(fetchUrl,configObj).then(res => res.json()).then(data => {
+       
+    
+       ulList.insertAdjacentHTML("beforebegin", `
+       this is the id num ${data.id}
+       `)
+       
+       console.log(data)
+   }).catch(error => console.log(`error: ${error}`))
+}
